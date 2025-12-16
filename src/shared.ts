23d86637b37c01f3d9ecd22522165c1b6828d57c
iHNSL <idohaveaname16@gmail.com>
2025-12-16 17:06:00 +0100

    changed names and some site components
    as title says, instead of boring names for the chat now it has roman emperors, greek gods and some generals from 1900-2000 as well as a more personal opener

--- src/shared.ts
+++ src/shared.ts
@@ -7,49 +7,75 @@ export type ChatMessage = {
 
 export type Message =
 	| {
-			type: "add";
-			id: string;
-			content: string;
-			user: string;
-			role: "user" | "assistant";
-	  }
+		type: "add";
+		id: string;
+		content: string;
+		user: string;
+		role: "user" | "assistant";
+	}
 	| {
-			type: "update";
-			id: string;
-			content: string;
-			user: string;
-			role: "user" | "assistant";
-	  }
+		type: "update";
+		id: string;
+		content: string;
+		user: string;
+		role: "user" | "assistant";
+	}
 	| {
-			type: "all";
-			messages: ChatMessage[];
-	  };
+		type: "all";
+		messages: ChatMessage[];
+	};
 
 export const names = [
-	"Alice",
-	"Bob",
-	"Charlie",
-	"David",
-	"Eve",
-	"Frank",
-	"Grace",
-	"Heidi",
-	"Ivan",
-	"Judy",
-	"Kevin",
-	"Linda",
-	"Mallory",
-	"Nancy",
-	"Oscar",
-	"Peggy",
-	"Quentin",
-	"Randy",
-	"Steve",
-	"Trent",
-	"Ursula",
-	"Victor",
-	"Walter",
-	"Xavier",
-	"Yvonne",
-	"Zoe",
+	// Roman Emperors
+	"Augustus",
+	"Trajan",
+	"Marcus Aurelius",
+	"Hadrian",
+	"Constantine",
+	"Tiberius",
+	"Nero",
+	"Caligula",
+	"Vespasian",
+	"Titus",
+	"Domitian",
+	"Commodus",
+	"Septimius Severus",
+	"Caracalla",
+	"Diocletian",
+
+	// Greek Gods & Mythology
+	"Zeus",
+	"Poseidon",
+	"Hades",
+	"Ares",
+	"Apollo",
+	"Hermes",
+	"Hephaestus",
+	"Dionysus",
+	"Athena",
+	"Artemis",
+	"Hera",
+	"Demeter",
+	"Persephone",
+	"Hercules",
+	"Achilles",
+	"Odysseus",
+	// Generals (1900-2000)
+	"Dwight D. Eisenhower",
+	"George S. Patton",
+	"Douglas MacArthur",
+	"Bernard Montgomery",
+	"Erwin Rommel",
+	"Georgy Zhukov",
+	"John J. Pershing",
+	"Norman Schwarzkopf",
+	"Vo Nguyen Giap",
+	"Chester Nimitz",
+	"George Marshall",
+	"Omar Bradley",
+	"Heinz Guderian",
+	"Erich von Manstein",
+	"Isoroku Yamamoto",
+	"ADOLF HITLER",
+	"Colin Powell"
 ];
